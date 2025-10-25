75a90256c96540f87fab5ad504e0126bfe12a449
harishy0406 <harivelgm76@gmail.com>
2025-10-25 22:39:50 +0530

    Title Icon Added

--- remove_lovable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Remove lovable references from files
-find . -type f \( -name "*.js" -o -name "*.jsx" -o -name "*.ts" -o -name "*.tsx" -o -name "*.json" -o -name "*.html" \) -exec sed -i '' 's/lovable//gi' {}
